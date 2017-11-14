bbcee8afb909cf891450b36721c421e580eb985f
Raymond Wu <rwu6@stuy.edu>
2017-11-13 20:19:01 -0500

    Raymond's commit, Dothraki subclass of Protagonist

---
+++ v3/Dothraki.java
@@ -0,0 +1,48 @@
+// Raymond Wu (with Thet Zaw, Kerwin Chen -- team TRK)
+// APCS1 pd1
+// HW31 -- Ye Olde Role Playing Game, Expanded
+// 2017-11-13
+
+public class Dothraki extends Protagonist
+{
+  // ~~~~~~~~~~~ INSTANCE VARIABLES ~~~~~~~~~~~
+  private String _name = "Haj Lajak";
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+  /*=============================================
+    default constructor
+    pre:  instance vars are declared
+    post: initializes instance vars.
+    =============================================*/
+  public Dothraki()
+  {
+    _hitPts = 180;
+    _strength = 1000;
+    _defense = 40;
+    _attack = .4;
+  }
+
+
+  /*=============================================
+    overloaded constructor
+    pre:  instance vars are declared
+    post: initializes instance vars. _name is set to input String.
+    =============================================*/
+  public Dothraki( String name )
+  {
+    this();
+    _name = name;
+  }
+
+
+  /*=============================================
+    String about() -- Who am I?
+      returns String to describe this type of Protagonist.
+    =============================================*/
+  public static String about()
+  {
+    return "Dothraki warriors are strong. Dothraki warriors can hit. Their special attribute is _strength.";
+  } // end about()
+
+} // end Protagonist > Dothraki class
